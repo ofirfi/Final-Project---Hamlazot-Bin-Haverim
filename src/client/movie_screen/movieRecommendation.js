b78b246c558836d8174e1dc0e14661a7db6defdc
ofirfi <OfirFital@gmail.com>
2021-02-28 22:14:04 +0200

    fixed issue with movies recommendations display

--- src/client/movie_screen/movieRecommendation.js
+++ src/client/movie_screen/movieRecommendation.js
@@ -20,7 +20,7 @@ export function Recommendations(props){
         {userName:"עידן המלך",rate:4,comment:"הערה 4",date:"8-10-2020"},
         {userName:"עידן הנסיך",rate:5,comment:"הערה 5",date:"9-10-2020"},
         {userName:"עידנכוכוס",rate:6,comment:"הערה 6",date:"25-10-2020"},
-        {userName:"test",rate:6,comment:"something",date:"25-10-2020"},
+        {userName:"test",rate:6,comment:"something very very very vyerrry loong not long enough or maybe it does now, not sure",date:"25-10-2020"},
 
     ]
 
@@ -35,11 +35,24 @@ export function Recommendations(props){
 
     const createRecommendation = (rec) =>{
         
-        let newRec = <div class = "flex flex-col mb-5 box-border border-2 bg-red-400 rounded-xl text-xs sm:text-sm ms:text-base">
-                <div>ממליץ:{'\t'+rec.userName}</div>
-                <div>דירוג: {'\t'+rec.rate}</div>
-                <div>הערה: {'\t'+rec.comment}</div>
-                <div>תאריך:{'\t'+rec.date}</div>
+        let newRec = 
+            <div class = "flex flex-col mb-5 box-border border-2 bg-red-400 rounded-xl text-xs sm:text-sm ms:text-base">
+                <div class = "flex flex-row-reverse">
+                    <div class = "w-1/6">ממליץ</div>
+                    <div class = "w-5/6">{rec.userName}</div>
+                </div>
+                <div class = "flex flex-row-reverse">
+                    <div class = "w-1/6">דירוג</div>
+                    <div class = "w-5/6">{rec.rate}</div>
+                </div>
+                <div class = "flex flex-row-reverse">
+                    <div class = "w-1/6">תאריך</div>
+                    <div class = "w-5/6">{rec.date}</div>
+                </div>
+                <div class = "flex flex-col">
+                    <div class = "">:תגובה</div>
+                    <div class = "h-12 overflow-y-auto">{rec.comment}</div>
+                </div>
             </div>
 
         setMovieRecommendations(old => [...old, newRec])
@@ -47,7 +60,7 @@ export function Recommendations(props){
 
 
     return(
-        <div class = "">
+        <div class = "grid flex">
             {movieRecommendations}
         </div>
     )
