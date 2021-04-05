79a0e382a1f6af0b229eaba89bf13d825260b894
ofirfi <OfirFital@gmail.com>
2021-04-05 21:51:10 +0300

    changed Place screen to get data from the API and refactored server's side code

--- src/server/Controllers/placesController.js
+++ src/server/Controllers/placesController.js
@@ -1,34 +1,35 @@
 const catchAsync = require('../utils/catchAsync'),
     config = require('config'),
     https = require('https');
-    
+
+
+const googleResponse = (res, resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => data += chunk);
+
+    // The whole response has been received.
+    resp.on('end', () => res.status(200).json(JSON.parse(data)));
+}
+
+const googleError = (res, err) => res.status(500).json({ err })
+
 
 module.exports = {
 
     search_places: catchAsync(async (req, res, next) => {
-        https.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?key=${config.PLACE_API_KEY}&input=${req.params.toSearch}`,
-            (resp) => {
-                let data = '';
-                // A chunk of data has been received.
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-
-                // The whole response has been received.
-                resp.on('end', () => {
-                    res.status(200).json(JSON.parse(data));
-                });
-
-            })
-            .on("error", (err) => {
-                console.error(err);
-                res.status(500).json({ err })
-            });
+        https.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?key=${config.PLACE_API_KEY}&input=${req.params.toSearch}&language=he`,
+            (resp) => googleResponse(res, resp))
+            .on("error", (err) => googleError(res, err))
 
     }),
 
+    
     get_place: catchAsync(async (req, res, next) => {
-        console.log('not ready yet');
-        res.status(501).send("not ready yet");
-    })
+        https.get(`https://maps.googleapis.com/maps/api/place/details/json?key=${config.PLACE_API_KEY}&place_id=${req.params.placeId}&language=he`,
+            (resp) => googleResponse(res, resp))
+            .on("error", (err) => googleError(res, err))
+    }),
+
 }
