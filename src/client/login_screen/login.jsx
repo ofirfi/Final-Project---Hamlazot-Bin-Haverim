10a1522f75238ecefd198d4dfb1af79f14899f6e
ofirfi <OfirFital@gmail.com>
2021-04-05 12:57:55 +0300

    Changed Login screen display

--- src/client/login_screen/login.jsx
+++ src/client/login_screen/login.jsx
@@ -66,55 +66,59 @@ const LoginPage = () => {
 
 
   return (
-    <div className="flex flex-col bg-fixed w-full"
+    <div className="flex flex-col bg-fixed w-full min-h-full"
       style={{ backgroundImage: `url(${BackGround})`, backgroundSize: '100% 100%' }}
     >
-      <header className="flex flex-col h-1/6 sm:h-2/6 w-full justify-center">
-        <img src={Header} alt="" className="h-full" />
+      <header className="flex flex-col sm:w-full h-0 sm:h-1/6 invisible sm:visible">
+        <img src={Header} alt="" className="" />
       </header>
 
 
 
-      <div className="flex flex-col self-center w-1/2 h-56 sm:h-1/2 sm:w-1/4 sm:h-3/4 bg-red-400 rounded-lg shadow-xl text-center items-center my-28">
+      <div className="flex flex-col self-center w-72 h-72 sm:h-3/4 my-6 sm:my-28 bg-red-400 rounded-md shadow-xl text-center items-center">
 
-        <div id="top" className="mb-3">
+        <div className="my-3 underline font-black text-xl">
           !ברוך הבא
         </div>
 
-        <div className="flex flex-col text-center items-center sm:space-y-3">
-          <div>
-            <input className="bg-blue-100 text-black placeholder-black sm:mt-2 text-center rounded"
+        <div className="flex flex-col text-center items-center mb-3 w-full h-full">
+
+          <div className="w-full h-12 flex flex-col items-center grid justify-items-center text-center">
+            <input className="bg-blue-100 text-black placeholder-black sm:mt-2 text-center rounded focus:outline-none"
               placeholder="מייל"
               value={email}
               onChange={event => setEmail(event.target.value)}
               required />
           </div>
 
-          <div>
-            <input className="bg-blue-100 text-black placeholder-black mt-2 text-center rounded flex flex-col"
+          <div className = "w-full h-12 flex flex-col items-center grid justify-items-center text-center">
+            <input className="bg-blue-100 text-black placeholder-black sm:mt-2 text-center rounded focus:outline-none"
               placeholder="סיסמא"
               type="password"
               value={password}
               onChange={event => setPassword(event.target.value)}
               required />
 
-            <text className="text-xs text-white sm:w-1/2"
+          </div>
+          
+          <div className="h-5 w-full flex flex-col items-center">
+            <button className="text-xs text-white focus:outline-none"
               onClick={forgot_password}
             >
               ?שכחת סיסמא
-            </text>
+            </button>
           </div>
 
-          <div>
-            <button className="border-1 sm:border-4 rounded-full mt-3 sm:py:3 sm:px-6 focus:ring-4 focus:ring-white focus:ring-opacity-50 bg-indigo-500 border-none text-white"
+          <div className="h-12 w-full mt-4">
+            <button className="w-1/2 h-3/4 rounded-lg text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:ring-white focus:ring-opacity-50 focus:outline-none"
               onClick={login}
             >
               התחבר
             </button>
           </div>
 
-          <div>
-            <button className="border-1 sm:border-4 rounded-full mt-3 sm:py:3 sm:px-6 focus:ring-4 focus:ring-white focus:ring-opacity-50 bg-indigo-500 border-none text-white"
+          <div className="h-12 w-full my-2">
+            <button className="w-1/2 h-3/4 sm:py:3 rounded-lg text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:ring-white focus:ring-opacity-50 focus:outline-none"
               onClick={signup}
             >
               הרשם
