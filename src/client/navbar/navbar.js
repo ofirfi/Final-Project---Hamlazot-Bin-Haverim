074b13be9a920b5702503297e29439ad3cd24a61
ofirfi <OfirFital@gmail.com>
2021-05-03 19:00:53 +0300

    Navbar animation is finally working. Tailwind update

--- src/client/navbar/navbar.js
+++ src/client/navbar/navbar.js
@@ -1,11 +1,10 @@
 import '../utils/style.css'
+import './navbar.css'
 import Header from '../images/logo.jpg'
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import checkAuth from '../utils/auth'
-
-
 import Hamburger from 'hamburger-react'
 
 
@@ -32,66 +31,63 @@ export function Navbar() {
     const tokenIsValid = () => {
         if (!checkAuth()) {
             clear();
-            alert('פג תוקף ההתחברות, אנא התחבר שוב');
             return false
         }
         return true
     }
-
 
     const relog = () => {
         alert('אנא התחבר מחדש')
         history.push('/login')
     }
 
-
     const profilePage = () => !tokenIsValid() ? relog() : history.push('/profile')
 
     const homePage = () => !tokenIsValid() ? relog() : history.push('')
 
+    const toggleNav = () => {
+        if (!isOpened)
+            document.getElementById("NavMenu").style.width = "100px";
+        else
+            document.getElementById("NavMenu").style.width = "0";
+        setIsOpened(!isOpened)
+    }
+
 
     return (
-        <div className="flex h-28 sm:h-28 lg:h-52 w-full grid"
+        <div className="flex h-28 sm:h-28 md:h-44 lg:h-52 w-full grid"
             style={{ backgroundImage: `url(${Header})`, backgroundSize: '100% 100%' }}
         >
+            <div className="flex flex-col w-24 h-full lg:pt-5 self-start text-white text-sm md:text-md lg:text-lg justify-self-end items-center">
+                <Hamburger
+                    easing="ease-in"
+                    toggled={isOpened}
+                    onToggle={toggleNav}
+                />
+            </div>
 
-            {isOpened ?
-                <div className="flex flex-col w-36 h-full lg:pt-5 self-start text-white text-sm md:text-md lg:text-lg justify-self-end items-center bg-gray-400">
-                    <Hamburger className="hover:text-red-400"
-                        easing="ease-in"
-                        toggled={isOpened}
-                        onToggle={() => setIsOpened(!isOpened)}
-                    />
+            <div id="NavMenu" className="flex flex-col h-16 md:h-32 self-start items-center grid justify-self-end jusitfy-items-center text-white text-sm md:text-md lg:text-lg bg-black bg-opacity-75">
 
-                    <button className="hover:text-gray-500 focus:outline-none"
-                        onClick={() => homePage()}
-                    >
-                        דף הבית
-                    </button>
-                    <button className="hover:text-gray-500 focus:outline-none"
-                        onClick={() => profilePage()}
-                    >
-                        פרופיל
-                    </button>
-                    <button className="hover:text-gray-500 focus:outline-none"
-                        onClick={() => log_out()}
-                    >
-                        יציאה
-                    </button>
-                </div>
+                <button className="transition ease-in-out duration-700 hover:text-gray-500 focus:outline-none"
+                    onClick={() => homePage()}
+                >
+                    דף הבית
+                 </button>
 
-                :
+                <button className="transition ease-in-out duration-700 hover:text-gray-500 focus:outline-none"
+                    onClick={() => profilePage()}
+                >
+                    פרופיל
+                </button>
 
-                <div className="flex flex-col w-36 h-full lg:pt-5 self-start text-white text-sm md:text-md lg:text-lg justify-self-end items-center">
-                    <Hamburger
-                        easing="ease-in"
-                        toggled={isOpened}
-                        onToggle={() => setIsOpened(!isOpened)}
-                    >
-                    </Hamburger>
-                </div>
+                <button className="transition ease-in-out duration-700 hover:text-gray-500 focus:outline-none"
+                    onClick={() => log_out()}
+                >
+                    יציאה
+                </button>
 
-            }
+            </div>
+
         </div >
     )
 
