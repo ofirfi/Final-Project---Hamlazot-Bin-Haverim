0e8c485697bc30955e669c21d7d6ddd6d51ded41
Idan Cohen <idance@post.jce.ac.il>
2021-05-23 23:01:00 +0300

    footer upgraded

--- src/client/footer/footer.js
+++ src/client/footer/footer.js
@@ -5,8 +5,19 @@ import '../utils/style.css'
 const Footer = () => {
 
     return (
-        <div className=" w-full underline mt-72 bottom-0 ">
-            <a target="_blank" href="https://forms.gle/VGueMnszdPhMoGo17">נשמח שתשאירו פידבק ע"י לחיצה כאן</a>
+        // <div className=" w-full underline mt-72 bottom-0 ">
+        //     <a target="_blank" href="https://forms.gle/VGueMnszdPhMoGo17">נשמח שתשאירו פידבק ע"י לחיצה כאן</a>
+        // </div>
+
+        <div class="bg-gray-600 w-full mt-60 bottom-0">
+            <footer class="w-screen flex flex-wrap items-center justify-between p-3 m-auto">
+                <div class="container mx-auto w-full flex flex-col flex-wrap items-center justify-between">
+                    <a className="text-white underline" target="_blank" href="https://forms.gle/VGueMnszdPhMoGo17">נשמח שתשאירו פידבק ע"י לחיצה כאן</a>
+                    <div class="flex mx-auto text-white text-center">
+                        כל הזכויות שמורות להמלצות בין חברים @ 2021
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
