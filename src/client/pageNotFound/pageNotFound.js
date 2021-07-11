f1a337f3ab07a257890008fec3f389fb121c1d3e
ofirfi <OfirFital@gmail.com>
2021-07-11 15:25:33 +0300

    redesigned 404

--- src/client/pageNotFound/pageNotFound.js
+++ src/client/pageNotFound/pageNotFound.js
@@ -1,16 +1,27 @@
 import './pageNotFound.css';
 import React from 'react';
-import {useHistory} from 'react-router-dom'
-
+import { useHistory } from 'react-router-dom'
+import Image from '../images/No-Reception.jpg'
 
 const PageNotFound = () => {
     const history = useHistory()
     return (
-        <div>
-            <h1>404 NOT FOUND!</h1>
-            <i  onClick={() => history.push('') }>
-            Click here to return to the previous page
+        <div className="flex flex-col p-32">
+            <div className="flex flex-row">
+                <div>
+                    <img src={Image} alt=""></img>
+                </div>
+                <div className="flex flex-col text-3xl font-black">
+                    <h2 className = "h-1/2 flex items-end">OPS...</h2>
+                    <h1 className = "h-1/2 flex items-start">404 NOT FOUND!</h1>
+                </div>
+            </div>
+            <i className = "underline font-mono"
+                onClick={() => history.push('')}
+                >
+                לחזרה לדף הראשי לחץ כאן
             </i>
+
         </div>
     );
 };
